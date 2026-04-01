00d607bcc925825d1179158fafd3a1886e7032b4
Nourdine84 <msanourdine@hotmail.com>
2026-04-01 12:10:20 +0200

    final fix prisma build vs runtime

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,30 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+let prisma: PrismaClient;
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
+function createPrismaClient() {
+  return new PrismaClient({
     log: ["error"],
   });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
 }
+
+/**
+ * 🔥 CRITIQUE : empêche Prisma pendant build Vercel
+ */
+if (process.env.NODE_ENV === "production" && process.env.VERCEL) {
+  // 👉 pendant le build, Next n’a PAS DATABASE_URL
+  if (!process.env.DATABASE_URL) {
+    console.log("⛔ Prisma skipped (no DATABASE_URL during build)");
+    prisma = {} as PrismaClient;
+  } else {
+    prisma = createPrismaClient();
+  }
+} else {
+  // dev ou runtime
+  if (!(global as any).prisma) {
+    (global as any).prisma = createPrismaClient();
+  }
+  prisma = (global as any).prisma;
+}
+
+export { prisma };
