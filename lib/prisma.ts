bfddd3e4b7bd74a3d25501529061e18cdcdd7e5d
Nourdine84 <msanourdine@hotmail.com>
2026-04-02 12:37:50 +0200

    fix prisma accelerate FINAL

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,22 +5,33 @@ const globalForPrisma = globalThis as unknown as {
 };
 
 /**
- * 🔥 DETECTION BUILD FIABLE
+ * 🔥 DETECTION BUILD (évite crash Vercel)
  */
 const isBuild =
   process.env.NEXT_PHASE === "phase-production-build";
 
 /**
- * 🔥 INSTANCE SAFE
+ * 🔥 URL PRIORITAIRE
+ */
+const databaseUrl =
+  process.env.PRISMA_ACCELERATE_URL ||
+  process.env.DATABASE_URL;
+
+/**
+ * 🔥 INSTANCE PRISMA
  */
 export const prisma =
   isBuild
-    ? ({} as PrismaClient) // ⛔ bloque Prisma au build
+    ? ({} as PrismaClient)
     : globalForPrisma.prisma ??
       new PrismaClient({
+        datasourceUrl: databaseUrl,
         log: ["error"],
       });
 
+/**
+ * 🔥 CACHE DEV
+ */
 if (!isBuild && process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
